3bb0f4c7c3d77987df3875b7261109bb2e21230e
badmaashbot <badmaashdil77@gmail.com>
2025-12-01 08:12:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,9 +51,9 @@ SYMBOL_MAP = {
 }
 
 WS_URL = (
-    "wss://stream-testnet.bybit.com/v5/public"
+    "wss://stream.bybit.com/v5/public/linear"
     if TESTNET
-    else "wss://stream.bybit.com/v5/public"
+    else "wss://stream-testnet.bybit.com/v5/public/linear"
 )
 
 # --------------- TRADING CONFIG -----------------
